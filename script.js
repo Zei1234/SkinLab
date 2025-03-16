62b2a5b3c1965e103120613e06a419956fb22ff9
Zei1234 <133249997+Zei1234@users.noreply.github.com>
2025-03-16 22:36:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function Redirect(){
+window.location = "shop.html"
+}
+
+document.getElementById("login-btn").addEventListener("click", function () {
+    document.getElementById("uname").value = "";
+    document.getElementById("psw").value = "";
+    
+});
